c38dccb4fca29a35e1d6c69047db0ffe9a0feccf
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-08 11:46:15 -0400

    create a card

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+class Card extends Component {
+  render() {
+    return <div className="Card">Some Stuff</div>;
+  }
+}
+
+export default Card;
